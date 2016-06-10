f9746f40130448d5278e44ceeb0abb0ea6670147
Orm <ormancalagon@gmail.com>
2016-06-10 22:59:25 +0100

    testing update

--- channelTest.py
+++ channelTest.py
@@ -6,6 +6,10 @@ GPIO.setup(17, GPIO.OUT)
 
 measurement = input("Please enter you number here: ")
 measurement = float(measurement) 
+try:
+	float(measurement)
+except ValueError:
+	print("you need to enter a floating-point number")
 
 GPIO.output(17, GPIO.HIGH)
 sleep(measurement)
