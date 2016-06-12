7b27df5acd5f66622d82b2eb54b3142e64626866
Samuel <sam.attwell@gmail.com>
2016-06-12 18:57:32 +0000

    Actually working float test

--- channelTest.py
+++ channelTest.py
@@ -3,15 +3,25 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(17, GPIO.OUT)
 
+measurement = 0
 
-measurement = input("Please enter you number here: ")
-measurement = float(measurement) 
-try:
-	float(measurement)
-except ValueError:
-	print("you need to enter a floating-point number")
+def getMeasurement():
+	while True:
+		try:
+			global measurement
+			measurement = input("Please enter you number here: ")
+			measurement = float(measurement)
+		except ValueError:
+			print("you need to enter a floating-point number")
+			continue
+		else:
+			break
 
-GPIO.output(17, GPIO.HIGH)
-sleep(measurement)
-GPIO.output(17, GPIO.LOW)
-GPIO.cleanup()
+def powerPin():
+	GPIO.output(17, GPIO.HIGH)
+	sleep(measurement*2)
+	GPIO.output(17, GPIO.LOW)
+
+getMeasurement()
+powerPin()
+GPIO.cleanup() 
